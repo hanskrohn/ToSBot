61711cb67bbadc4a8b3f3b93affa6fa9a99d2ddc
Hans Krohn <hanskrohn70@gmail.com>
2021-07-07 21:31:41 -0400

    README.md and boiler for Flask API

--- Scrapper/schema.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const UnParsedDataSchema = new Schema({
-
-}, { strict: false });
-
-module.exports = mongoose.model('UnParsedData', UnParsedDataSchema);

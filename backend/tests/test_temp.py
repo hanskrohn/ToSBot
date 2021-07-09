9debea10042709031c6b8fa670d2078733f7129c
Hans Krohn <hanskrohn70@gmail.com>
2021-07-09 19:53:16 -0400

    pass python tests

--- backend/tests/test_temp.py
+++ backend/tests/test_temp.py
@@ -6,7 +6,7 @@ from controller.temp import TempClass
 class TempTestCase(unittest.TestCase):
     # testing
     def test_get_response(self):
-        expected_result = {"data": "hello World"}
+        expected_result = {"data": "Hello World"}
         self.assertEqual(TempClass.HelloWorld(), expected_result)
 
 

09be390c1087d2db42a1dc39af21f2c0525e69f1
Hans Krohn <hanskrohn70@gmail.com>
2021-07-09 18:55:48 -0400

    testing

--- backend/tests/test_temp.py
+++ backend/tests/test_temp.py
@@ -4,7 +4,6 @@ import requests
 from controller.temp import TempClass
 
 class TempTestCase(unittest.TestCase):
-    # comment to test tests
     def test_get_response(self):
         expected_result = {"data": "Hello World"}
         self.assertEqual(TempClass.HelloWorld(), expected_result)

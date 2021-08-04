1f0823d479c4895e3415e8974aaae1ff5fb2c418
Hans Krohn <hanskrohn70@gmail.com>
2021-08-04 16:43:40 -0400

    Created the main containers and fixed some possible linting problems

---
+++ frontend/src/components/Summary.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import '../css/Summary.css';
+
+const Summary: React.FC<any> = () => {
+  return <div className="summary-container grey-container-borders"> Hello World</div>;
+};
+
+export { Summary };

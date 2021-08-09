dd67aeb28cd383e60b3f2bcd896449ec10bbbf68
Hans Krohn <hanskrohn70@gmail.com>
2021-08-09 17:48:01 -0400

    Add card expandable complete, missing voting system on cards

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -2,31 +2,74 @@ import React from 'react';
 
 import '../css/Card.css';
 import { Grade, Grades } from './Grade';
+import { HiOutlineArrowDown, HiOutlineArrowUp } from 'react-icons/hi';
+import { useState } from 'react';
+
+const expand: JSX.Element = (
+  <>
+    <p>Click to expand&nbsp;</p>
+    <HiOutlineArrowDown />
+  </>
+);
+
+const collapse: JSX.Element = (
+  <>
+    <p>Click to collapse&nbsp;</p>
+    <HiOutlineArrowUp />
+  </>
+);
+
+export interface CardProps {
+  caseBlur: string;
+  grade: Grades;
+  quote: string;
+}
 
 /** Card component to display card */
-const Card: React.FC = () => {
+const Card: React.FC<CardProps> = (props: CardProps) => {
+  const { caseBlur, grade, quote } = props;
+  const [expandableButton, setExpandableButton] = useState<JSX.Element>(expand);
+
+  /** Change the direction of the expandable button. Expand -> Collapse Collapse -> Expand */
+  const changeExpandableButton = (): void => {
+    if (expandableButton === expand) {
+      setExpandableButton(collapse);
+      return;
+    }
+    setExpandableButton(expand);
+  };
+
   return (
     <div className="card">
-      <div className="card-content-container">
+      <div className="card-content-container flex">
         <div className="card-subtitle">
           <h5>Case:</h5>
         </div>
         <div className="card-content">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos accusantium architecto sint facere! Sed
-            quos consectetur incidunt mollitia rem dignissimos nulla ullam cum iure veniam harum officia dicta, fugiat
-            ex.
-          </p>
+          <p>{caseBlur}</p>
         </div>
       </div>
-      <div className="card-content-container">
+      <div className="card-content-container flex">
         <div className="card-subtitle">
           <h5>Grade:</h5>
         </div>
         <div className="card-content">
-          <Grade grade={Grades.Good} />
+          <Grade grade={grade} />
+          <div className="expand-card " role="button" onClick={changeExpandableButton}>
+            {expandableButton}
+          </div>
         </div>
       </div>
+      {expandableButton === collapse && (
+        <div className="card-content-container flex">
+          <div className="card-subtitle">
+            <h5>Quote:</h5>
+          </div>
+          <div className="card-content">
+            <p>{quote}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };

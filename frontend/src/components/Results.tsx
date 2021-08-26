59e106cc545b87c5a399b2f4c88c207bde0224f1
Hans Krohn <hanskrohn70@gmail.com>
2021-08-26 17:41:24 -0400

    Got the sorting and filters done

--- frontend/src/components/Results.tsx
+++ frontend/src/components/Results.tsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import '../css/Results.css';
 import { Searching } from './Searching';
 import { Card, CardProps } from './Card';
 import { Grades } from './Grade';
+import { OrderByStates } from './OrderBy';
 import { Filters } from './Filters';
 
 /** Components that displays the result cards of the TOS */
 const Results: React.FC = () => {
+  const [filterBy, setFilterBy] = useState<Set<Grades>>(new Set<Grades>([Grades.Good, Grades.Bad, Grades.Neutral]));
+  const [orderBy, setOrderBy] = useState<OrderByStates>(OrderByStates.AsTheyAppear);
+
   /** TODO: This function will be removed and replaced with actual grades attached to each case */
+  const createList = () => {
+    const list = [];
+    for (let i = 0; i < 100; i++) {
+      list.push({
+        caseBlur: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos accusantium architecto sint facere!
+        Sed quos consectetur incidunt mollitia rem dignissimos nulla ullam cum iure veniam harum officia dicta,
+        fugiat ex.`,
+        grade: getRandomGrade(),
+        quote: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos accusantium architecto sint facere!
+        Sed quos consectetur incidunt mollitia rem dignissimos nulla ullam cum iure veniam harum officia dicta,
+        fugiat ex.`,
+      });
+    }
+
+    return list;
+  };
+
   const getRandomGrade = (): Grades => {
     const cases = {
       0: Grades.Good,
@@ -17,163 +38,93 @@ const Results: React.FC = () => {
     };
     const num = Math.floor(Math.random() * 3);
     return cases[num] ?? Grades.Neutral;
+  }; // TODO: This will removed and replaced with actuall cases
+  const [cards, setCards] = useState<CardProps[]>(createList());
+
+  /** Function to filter the list of cards by the selected filters */
+  const filterTempCards = (): CardProps[] => {
+    const newList: CardProps[] = [];
+
+    cards.forEach((card) => {
+      if (filterBy.has(card.grade)) {
+        newList.push(card);
+      }
+    });
+
+    return newList;
   };
-  // TODO: This will removed and replaced with actuall cases
-  const tempCards: CardProps[] = [
-    {
-      caseBlur: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos accusantium architecto sint facere!
-      Sed quos consectetur incidunt mollitia rem dignissimos nulla ullam cum iure veniam harum officia dicta,
-      fugiat ex.`,
-      grade: getRandomGrade(),
-      quote: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos accusantium architecto sint facere!
-      Sed quos consectetur incidunt mollitia rem dignissimos nulla ullam cum iure veniam harum officia dicta,
-      fugiat ex.`,
-    },
-    {
-      caseBlur: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos accusantium architecto sint facere!
-      Sed quos consectetur incidunt mollitia rem dignissimos nulla ullam cum iure veniam harum officia dicta,
-      fugiat ex.`,
-      grade: getRandomGrade(),
-      quote: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos accusantium architecto sint facere!
-      Sed quos consectetur incidunt mollitia rem dignissimos nulla ullam cum iure veniam harum officia dicta,
-      fugiat ex.`,
-    },
-    {
-      caseBlur: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos accusantium architecto sint facere!
-      Sed quos consectetur incidunt mollitia rem dignissimos nulla ullam cum iure veniam harum officia dicta,
-      fugiat ex.`,
-      grade: getRandomGrade(),
-      quote: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos accusantium architecto sint facere!
-      Sed quos consectetur incidunt mollitia rem dignissimos nulla ullam cum iure veniam harum officia dicta,
-      fugiat ex.`,
-    },
-    {
-      caseBlur: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos accusantium architecto sint facere!
-      Sed quos consectetur incidunt mollitia rem dignissimos nulla ullam cum iure veniam harum officia dicta,
-      fugiat ex.`,
-      grade: getRandomGrade(),
-      quote: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos accusantium architecto sint facere!
-      Sed quos consectetur incidunt mollitia rem dignissimos nulla ullam cum iure veniam harum officia dicta,
-      fugiat ex.`,
-    },
-    {
-      caseBlur: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos accusantium architecto sint facere!
-      Sed quos consectetur incidunt mollitia rem dignissimos nulla ullam cum iure veniam harum officia dicta,
-      fugiat ex.`,
-      grade: getRandomGrade(),
-      quote: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos accusantium architecto sint facere!
-      Sed quos consectetur incidunt mollitia rem dignissimos nulla ullam cum iure veniam harum officia dicta,
-      fugiat ex.`,
-    },
-    {
-      caseBlur: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos accusantium architecto sint facere!
-      Sed quos consectetur incidunt mollitia rem dignissimos nulla ullam cum iure veniam harum officia dicta,
-      fugiat ex.`,
-      grade: getRandomGrade(),
-      quote: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos accusantium architecto sint facere!
-      Sed quos consectetur incidunt mollitia rem dignissimos nulla ullam cum iure veniam harum officia dicta,
-      fugiat ex.`,
-    },
-    {
-      caseBlur: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos accusantium architecto sint facere!
-      Sed quos consectetur incidunt mollitia rem dignissimos nulla ullam cum iure veniam harum officia dicta,
-      fugiat ex.`,
-      grade: getRandomGrade(),
-      quote: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos accusantium architecto sint facere!
-      Sed quos consectetur incidunt mollitia rem dignissimos nulla ullam cum iure veniam harum officia dicta,
-      fugiat ex.`,
-    },
-    {
-      caseBlur: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos accusantium architecto sint facere!
-      Sed quos consectetur incidunt mollitia rem dignissimos nulla ullam cum iure veniam harum officia dicta,
-      fugiat ex.`,
-      grade: getRandomGrade(),
-      quote: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos accusantium architecto sint facere!
-      Sed quos consectetur incidunt mollitia rem dignissimos nulla ullam cum iure veniam harum officia dicta,
-      fugiat ex.`,
-    },
-    {
-      caseBlur: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos accusantium architecto sint facere!
-      Sed quos consectetur incidunt mollitia rem dignissimos nulla ullam cum iure veniam harum officia dicta,
-      fugiat ex.`,
-      grade: getRandomGrade(),
-      quote: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos accusantium architecto sint facere!
-      Sed quos consectetur incidunt mollitia rem dignissimos nulla ullam cum iure veniam harum officia dicta,
-      fugiat ex.`,
-    },
-    {
-      caseBlur: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos accusantium architecto sint facere!
-      Sed quos consectetur incidunt mollitia rem dignissimos nulla ullam cum iure veniam harum officia dicta,
-      fugiat ex.`,
-      grade: getRandomGrade(),
-      quote: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos accusantium architecto sint facere!
-      Sed quos consectetur incidunt mollitia rem dignissimos nulla ullam cum iure veniam harum officia dicta,
-      fugiat ex.`,
-    },
-    {
-      caseBlur: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos accusantium architecto sint facere!
-      Sed quos consectetur incidunt mollitia rem dignissimos nulla ullam cum iure veniam harum officia dicta,
-      fugiat ex.`,
-      grade: getRandomGrade(),
-      quote: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos accusantium architecto sint facere!
-      Sed quos consectetur incidunt mollitia rem dignissimos nulla ullam cum iure veniam harum officia dicta,
-      fugiat ex.`,
-    },
-    {
-      caseBlur: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos accusantium architecto sint facere!
-      Sed quos consectetur incidunt mollitia rem dignissimos nulla ullam cum iure veniam harum officia dicta,
-      fugiat ex.`,
-      grade: getRandomGrade(),
-      quote: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos accusantium architecto sint facere!
-      Sed quos consectetur incidunt mollitia rem dignissimos nulla ullam cum iure veniam harum officia dicta,
-      fugiat ex.`,
-    },
-    {
-      caseBlur: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos accusantium architecto sint facere!
-      Sed quos consectetur incidunt mollitia rem dignissimos nulla ullam cum iure veniam harum officia dicta,
-      fugiat ex.`,
-      grade: getRandomGrade(),
-      quote: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos accusantium architecto sint facere!
-      Sed quos consectetur incidunt mollitia rem dignissimos nulla ullam cum iure veniam harum officia dicta,
-      fugiat ex.`,
-    },
-    {
-      caseBlur: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos accusantium architecto sint facere!
-      Sed quos consectetur incidunt mollitia rem dignissimos nulla ullam cum iure veniam harum officia dicta,
-      fugiat ex.`,
-      grade: getRandomGrade(),
-      quote: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos accusantium architecto sint facere!
-      Sed quos consectetur incidunt mollitia rem dignissimos nulla ullam cum iure veniam harum officia dicta,
-      fugiat ex.`,
-    },
-    {
-      caseBlur: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos accusantium architecto sint facere!
-      Sed quos consectetur incidunt mollitia rem dignissimos nulla ullam cum iure veniam harum officia dicta,
-      fugiat ex.`,
-      grade: getRandomGrade(),
-      quote: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos accusantium architecto sint facere!
-      Sed quos consectetur incidunt mollitia rem dignissimos nulla ullam cum iure veniam harum officia dicta,
-      fugiat ex.`,
-    },
-    {
-      caseBlur: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos accusantium architecto sint facere!
-      Sed quos consectetur incidunt mollitia rem dignissimos nulla ullam cum iure veniam harum officia dicta,
-      fugiat ex.`,
-      grade: getRandomGrade(),
-      quote: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos accusantium architecto sint facere!
-      Sed quos consectetur incidunt mollitia rem dignissimos nulla ullam cum iure veniam harum officia dicta,
-      fugiat ex.`,
-    },
-  ];
+
+  /** Determine order of sorting and sort the cards */
+  const sortCards = (cards: CardProps[]): CardProps[] => {
+    if (orderBy === OrderByStates.AsTheyAppear) {
+      return cards;
+    }
+
+    let order: Grades[];
+    if (orderBy === OrderByStates.GoodToBad) {
+      order = [Grades.Good, Grades.Neutral, Grades.Bad];
+    } else {
+      order = [Grades.Bad, Grades.Neutral, Grades.Good];
+    }
+
+    return sort(cards, order);
+  };
+
+  /**
+   * Sorts the cards[] according to the order provided. Only works if order is of length 3
+   *
+   * @param cards - CardProps[] you want to sort
+   * @param order - The order you want to sorth them in
+   * @returns - Sorted CardProps[]
+   */
+  const sort = (cards: CardProps[], order: Grades[]): CardProps[] => {
+    const firstValue = order[0];
+    const secondValue = order[1];
+
+    let firstIdx = 0;
+    let secondIdx = 0;
+    let thirdIdx = cards.length - 1;
+
+    while (secondIdx <= thirdIdx) {
+      const currCard = cards[secondIdx];
+
+      if (currCard.grade === firstValue) {
+        swap(firstIdx, secondIdx, cards);
+        firstIdx++;
+        secondIdx++;
+      } else if (currCard.grade === secondValue) {
+        secondIdx++;
+      } else {
+        swap(secondIdx, thirdIdx, cards);
+        thirdIdx--;
+      }
+    }
+
+    return cards;
+  };
+
+  /** Swap Cards in cards array */
+  const swap = (i: number, j: number, cards: CardProps[]): void => {
+    const temp = cards[i];
+    cards[i] = cards[j];
+    cards[j] = temp;
+  };
+
+  useEffect(() => {
+    const newCardList = filterTempCards();
+    const newCardOrder = sortCards(newCardList);
+
+    setCards([...newCardOrder]);
+  }, [filterBy, orderBy]);
 
   return (
     <div className="results-container grey-container-borders">
       <div className="filter-container">
-        <Filters />
+        <Filters setFilterBy={setFilterBy} filterBy={filterBy} setOrderBy={setOrderBy} />
       </div>
       {/* <Searching displaySearchingText /> */}
       <div className="results">
-        {tempCards.map((card, i) => {
+        {cards.map((card, i) => {
           return (
             <div key={i} className="margin-small">
               <Card caseBlur={card.caseBlur} grade={card.grade} quote={card.quote} />

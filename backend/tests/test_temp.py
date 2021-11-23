ec08175af438adf2e3604b4ba913ee68e109e112
Hans Krohn <hanskrohn70@gmail.com>
2021-11-22 19:33:08 -0500

    removed tests

--- backend/tests/test_temp.py
+++ backend/tests/test_temp.py
@@ -1,13 +1,11 @@
 import unittest
 import requests
 
-from controller.temp import TempClass
-
 class TempTestCase(unittest.TestCase):
     # testing
-    def test_get_response(self):
-        expected_result = {"data": "Hello World"}
-        self.assertEqual(TempClass.HelloWorld(), expected_result)
+    # def test_get_response(self):
+    #     expected_result = {"data": "Hello World"}
+    #     self.assertEqual(TempClass.HelloWorld(), expected_result)
 
 
 if __name__ == "__main__":

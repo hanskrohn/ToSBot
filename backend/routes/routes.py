a432c23c6208bff0e92cf712dc17a9e3c81bbd87
gbohm99 <grahambohm@gmail.com>
2021-12-04 19:40:44 -0500

    complete pipeline for case mapping from parsed sentences

--- backend/routes/routes.py
+++ backend/routes/routes.py
@@ -6,4 +6,5 @@ from controller.process_html import ProcessHTML
 class Routes(Resource):
     def post(self):
         data = request.json
+        print(data)
         return ProcessHTML.init(data['html'])

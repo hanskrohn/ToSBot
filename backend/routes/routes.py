4668d0f040199c067c69194bafbf4a64eb9cebc2
gbohm99 <grahambohm@gmail.com>
2021-12-08 12:11:02 -0500

    Fix absolute path error & response format error

--- backend/routes/routes.py
+++ backend/routes/routes.py
@@ -6,5 +6,5 @@ from controller.process_html import ProcessHTML
 class Routes(Resource):
     def post(self):
         data = request.json
-        print(data)
+        # print(data)
         return ProcessHTML.init(data['html'])

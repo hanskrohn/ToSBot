cc7a6360618d9b5801d8e52c6d2882e11f41803b
Jeffrey Chen <44081961+JefffffC@users.noreply.github.com>
2021-12-14 01:54:43 -0500

    replaced backend HTML cleaning with more robust frontend approach, minor CSS tweaks

--- backend/controller/process_html.py
+++ backend/controller/process_html.py
@@ -1,22 +1,10 @@
-from bs4 import BeautifulSoup
 from .annotate_terms import get_annotation
 
 class ProcessHTML:
     def init(html):
-      data = ProcessHTML.cleanHTML(html)
-      annotated_terms = get_annotation(data, 0.75)
+      print("THIS IS THE HTML:::")
+      print(html)
+      annotated_terms = get_annotation(html, 0.75)
       return {"cards" : annotated_terms}
     
-    def cleanHTML(html):
-      soup = BeautifulSoup(html, "html.parser") # create a new bs4 object from the html data loaded
-      for script in soup(["script", "style"]): # remove all javascript and stylesheet code
-        script.extract()
-      # get text
-      text = soup.get_text()
-      # break into lines and remove leading and trailing space on each
-      lines = (line.strip() for line in text.splitlines())
-      # break multi-headlines into a line each
-      chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-      # drop blank lines
-      text = '\n'.join(chunk for chunk in chunks if chunk)
-      return text
+# We take out bs4 since we are using client-side cleaning now (vanilla JS provides some good tools for this)

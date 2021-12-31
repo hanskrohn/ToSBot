f7677de02f7c7d45b0d2780d69f10c615f1e3da7
Jeffrey Chen <44081961+JefffffC@users.noreply.github.com>
2021-12-30 22:43:58 -0500

    added client-side caching system and UUID storage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
+/*global chrome*/
 import React, { useEffect, useState } from 'react';
-
+import { v4 as uuidv4 } from 'uuid';
 import './css/App.css';
 import { Summary } from './components/Summary';
 import { Results } from './components/Results';
 import { Searching } from './components/Searching';
 import { HighlightButton } from './components/HighlightButton';
-import { getDomContent } from './background.js';
-import { getHTML } from './htmlContent';
+import { getDomContent, getWebsiteURL } from './background.js';
+import { getHTML, getURL } from './helper';
 import { caseObject } from './types';
 
 /** Function to Load React App */
@@ -16,37 +17,93 @@ function App(): JSX.Element {
 
   useEffect(() => {
     const abortCtrl = new AbortController();
-    getDomContent().then(() => {
-      const html = getHTML();
-      const data = { html };
-      fetch('http://127.0.0.1:5000/api', {
-        method: 'POST',
-        headers: {
-          // eslint-disable-next-line @typescript-eslint/naming-convention
-          'Content-Type': 'application/json',
-        },
-        signal: abortCtrl.signal,
-        body: JSON.stringify(data),
-      }).then(
-        (res) => {
-          res.json().then((data) => {
-            const stateData: { cards: caseObject[] } = data;
-            // backend passes all sentences, we are only interested in sentences matched to cases
-            stateData.cards = stateData.cards.filter((obj) => obj.has_case === true);
-            // backend does not append this field but for FE state management we need it
-            for (const caseObj of stateData.cards) {
-              caseObj.has_voted = false;
-              caseObj.vote_type = null;
+
+    /* 
+      TOSBOT client-side caching solution to minimize API calls and store UUID
+    */
+    // check if user has a UUID and previously loaded cases for the website, if so, check the timestamp.
+    // only render from past data if < 1 day old (we want to avoid showing data from outdated TOS or models)
+    // this minimizes redundant API calls to our backend (and improves user experience)
+    getWebsiteURL().then(() => {
+      const TOSurl = getURL();
+      chrome.storage.local.get(['ToSBot-user-data'], (res) => {
+        console.log(res);
+        if (
+          res === undefined ||
+          (res && Object.keys(res).length === 0 && Object.getPrototypeOf(res) === Object.prototype)
+        ) {
+          // generate a new UUID and storage object for the user
+          chrome.storage.local.set(
+            {
+              'ToSBot-user-data': {
+                uuid: uuidv4(),
+                websites: {},
+              },
+            },
+            () => {
+              queryAPI();
             }
-            setCaseData(stateData);
-            setIsLoading(false);
-          });
-        },
-        (err) => {
-          console.error(err);
+          );
+        } else {
+          // user has previously used ToSBot, check if the website data is cached
+          // we use the TOS url as a lookup key, a future improvement would be company name
+          // eslint-disable-next-line no-prototype-builtins
+          if (res['ToSBot-user-data'].websites.hasOwnProperty(TOSurl)) {
+            // if the key is found, check if the cached data is not stale
+            if (res['ToSBot-user-data'].websites[TOSurl].timestamp > Date.now() - 86400000) {
+              setCaseData(res['ToSBot-user-data'].websites[TOSurl].caseData);
+              setIsLoading(false);
+            }
+          } else {
+            queryAPI();
+          }
         }
-      );
+      });
+
+      const queryAPI = () => {
+        getDomContent().then(() => {
+          const html = getHTML();
+          const data = { html };
+          fetch('http://127.0.0.1:5000/api', {
+            method: 'POST',
+            headers: {
+              // eslint-disable-next-line @typescript-eslint/naming-convention
+              'Content-Type': 'application/json',
+            },
+            signal: abortCtrl.signal,
+            body: JSON.stringify(data),
+          }).then(
+            (res) => {
+              res.json().then((data) => {
+                const stateData: { cards: caseObject[] } = data;
+                // backend passes all sentences, we are only interested in sentences matched to cases
+                stateData.cards = stateData.cards.filter((obj) => obj.has_case === true);
+                // backend does not append this field but for FE state management we need it
+                for (const caseObj of stateData.cards) {
+                  caseObj.has_voted = false;
+                  caseObj.vote_type = null;
+                }
+                setCaseData(stateData);
+                setIsLoading(false);
+
+                chrome.storage.local.get('ToSBot-user-data', (res) => {
+                  const resCopy = { ...res }['ToSBot-user-data'];
+                  resCopy.websites[TOSurl] = { timestamp: Date.now(), caseData: stateData };
+                  console.log(resCopy);
+                  // update user storage with latest data
+                  chrome.storage.local.set({ 'ToSBot-user-data': resCopy });
+                  // for
+                });
+              });
+            },
+            (err) => {
+              console.error(err);
+            }
+          );
+        });
+      };
     });
+
     return () => abortCtrl.abort();
   }, []);
 
@@ -57,7 +114,7 @@ function App(): JSX.Element {
           <Searching displaySearchingText={false} />
         </div>
       ) : (
-        <Summary cases={caseData.cards}/>
+        <Summary cases={caseData.cards} />
       )}
       {isLoading ? (
         <div id="results-loading" className="grey-container-borders">

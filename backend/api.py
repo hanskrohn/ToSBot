260bc068ee6b2b56465a8a665e66941386740b56
Jeffrey Chen <44081961+JefffffC@users.noreply.github.com>
2022-02-22 14:29:37 -0500

    add route for retraining

--- backend/api.py
+++ backend/api.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 
 from routes.cases import Cases
 from routes.votes import Votes
+from routes.retrain import Retrain
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -11,6 +12,7 @@ api = Api(app)
 
 api.add_resource(Cases, '/api')
 api.add_resource(Votes, '/vote')
+api.add_resource(Retrain, '/retrain')
 
 if __name__ == '__main__':
     app.run(debug=True)
